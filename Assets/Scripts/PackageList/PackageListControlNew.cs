4b23db0415cf6372e2388a434a59c5542756b822
Ivan Levarda <ivan.levarda@outlook.de>
2020-12-05 02:27:22 +0100

    Change background color of tourlist item according to the status of the package (done, missing, damaged)

--- Assets/Scripts/PackageList/PackageListControlNew.cs
+++ Assets/Scripts/PackageList/PackageListControlNew.cs
@@ -1,24 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using DataProvider;
+﻿using DataProvider;
 using Microsoft.MixedReality.Toolkit.Utilities;
 using TMPro;
+using UnityEngine;
 
 public class PackageListControlNew : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject itemTemplate;
+    [SerializeField] private GameObject itemTemplate;
 
-    [SerializeField]
-    private TextMeshPro titel;
+    [SerializeField] private TextMeshPro titel;
 
-    [SerializeField]
-    private GridObjectCollection packageCollection;
+    [SerializeField] private GridObjectCollection packageCollection;
+
+    private readonly Color32 failureColor = new Color32(247, 71, 71, 1);
+    private readonly Color32 soloplanColor = new Color32(173, 20, 93, 1);
+    private readonly Color32 successColor = new Color32(21, 175, 70, 1);
+    private readonly Color32 warningColor = new Color32(246, 231, 133, 1);
 
     private DataManager manager;
-    
+
 
     private void Start()
     {
@@ -33,20 +32,48 @@ public class PackageListControlNew : MonoBehaviour
         titel.text = "Tour: " + manager.currentTour.id;
         foreach (var package in manager.currentTour.ssccs)
         {
-            GameObject packageItem = Instantiate(itemTemplate, packageCollection.transform);
-            var textMesh = packageItem.transform.Find("Title").gameObject;
+            var packageUiItem = Instantiate(itemTemplate, packageCollection.transform);
+            var textMesh = packageUiItem.transform.Find("Title").gameObject;
             var packageText = textMesh.GetComponent<TextMeshPro>();
             packageText.text = "Paket\n" + package.code;
+            setBackgroundColorAccordingToPackageStatus(packageUiItem, packageText, package);
         }
+
         // hide the package item template
         Destroy(itemTemplate);
-        
+
         // update Layout 
         packageCollection.UpdateCollection();
     }
 
-    void RemoveList()
+    private void setBackgroundColorAccordingToPackageStatus(GameObject packageUiItem, TextMeshPro packageText,
+        PackageData package)
     {
-       
+        var backPlate = packageUiItem.transform.Find("BackPlate").gameObject;
+
+        // set color for currently scanned package
+        if (manager.currentPackage.code == package.code)
+            backPlate.GetComponent<Renderer>().material.color = soloplanColor;
+
+        var packageStatus = package.SSCCStatus;
+        switch (packageStatus)
+        {
+            case 1: // SSCCStatus = 1  means package is processed normally 
+                backPlate.GetComponent<Renderer>().material.color = successColor;
+                packageText.color = Color.black;
+                break;
+            case 2: // SSCCStatus = 2  means package is damaged 
+                backPlate.GetComponent<Renderer>().material.color = warningColor;
+                packageText.color = Color.black;
+                break;
+            case 3: // SSCCStatus = 3  means package is missing 
+                backPlate.GetComponent<Renderer>().material.color = failureColor;
+                // packageText.color = Color.black;
+                break;
+        }
+    }
+
+    private void RemoveList()
+    {
     }
 }
