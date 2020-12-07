eb80ecee9586a6320cc8a26c529a74256b4a1569
Ivan Levarda <ivan.levarda@outlook.de>
2020-12-07 03:25:45 +0100

    You can abbort a tour now and get to the main menu

---
+++ Assets/EndTour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTour : MonoBehaviour
+{
+    public void SetCurrentPackageToNull()
+    {
+        DataManager.Instance.currentPackage = null;
+    }
+}
